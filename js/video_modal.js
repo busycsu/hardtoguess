4468b28c5c08d047f3dbcd03d6232f2f2bb698b3
csu <imcssu@gmail.com>
2021-01-24 16:16:01 -0800

    video enlarge part

--- js/video_modal.js
+++ js/video_modal.js
@@ -1,6 +1,6 @@
 // Get the modal
 var modal = document.getElementById("myModal");
-            
+var v = document.getElementById("test")            
 var divs = document.getElementsByClassName('myImg');
 for (var i = 0; i < divs.length; i++) {
     var img = divs[i];
@@ -8,21 +8,29 @@ for (var i = 0; i < divs.length; i++) {
     // Get the image and insert it inside the modal - use its "alt" text as a caption
     var modalImg = document.getElementById("img01");
     var captionText = document.getElementById("caption");
-    img.onclick = function(){
+    divs[i].addEventListener("click",function(event){
         modal.style.display = "block";
         modalImg.src = this.src;
-        captionText.innerHTML = this.alt;
-    }
+        event.preventDefault();
+    }, false);
+    // img.onclick = function(){
+    //     modal.style.display = "block";
+        
+    //     // modalImg.src = this.src;
+    //     // captionText.innerHTML = this.alt;
+    // }
     
     // Get the <span> element that closes the modal
     var span = document.getElementsByClassName("close")[0];
     
     // When the user clicks on <span> (x), close the modal
-    span.onclick = function() { 
+    span.onclick = function() {
+        modalImg.pause(); 
         modal.style.display = "none";
     }
     window.onclick = function(event) {
         if (event.target == modal) {
+            modalImg.pause();
             modal.style.display = "none";
         }
     }
