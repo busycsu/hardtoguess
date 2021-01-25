959cd6d189722bb7fd7406b28b25609838a5377a
csu <imcssu@gmail.com>
2021-01-24 18:13:29 -0800

    email validation

--- js/modal.js
+++ js/modal.js
@@ -36,7 +36,7 @@ var mybutton = document.getElementById("myBtn");
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
