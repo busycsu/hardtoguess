2313a1a26aaada218fd76232d8b968351d11eb1a
csu <imcssu@gmail.com>
2021-01-26 19:12:26 -0800

    email check rule

--- js/email.js
+++ js/email.js
@@ -3,9 +3,16 @@ function ValidateEmail(inputText)
     var mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
     if(inputText.value.match(mailformat))
     {
-        alert("Valid email address!");
-        document.form1.text1.focus();
-        return true;
+        if(inputText.value.slice(inputText.value.length-3)=="edu"||inputText.value.slice(inputText.value.length-3)=="com"){
+            alert("Valid email address!");
+            document.form1.text1.focus();
+            return true;
+        }
+        else{
+            alert("You have entered an invalid email address!");
+            document.form1.text1.focus();
+            return false;
+        }
     }
     else
     {
